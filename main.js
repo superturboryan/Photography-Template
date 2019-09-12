50e8a74e0f5920b96398a1190f92f5d46ae017c9
Ryan David Forsyth <forsyth.r@gmail.com>
2019-09-11 20:57:11 -0400

    Add custom cursor, reduce img width, hide nav title

--- main.js
+++ main.js
@@ -12,16 +12,34 @@ const navOptions = {
 
 var navHeadroom = new Headroom(navBar, navOptions);
 // initialise
-navHeadroom.init();
+// navHeadroom.init();
 
 /////////////////////
 // DO THINGS WHEN PAGE OFFSET IS...
 window.onscroll = function () {
+
    var title = document.getElementById('title');
-   if (window.pageYOffset > 346) {
+
+   if (window.pageYOffset > 500) {
       title.classList.add("opacity-zero");
    } else {
       title.classList.remove("opacity-zero");
+   }
+
+   var titleOutline = this.document.getElementById('title-outline')
+
+   if (window.pageYOffset > 384) {
+      titleOutline.classList.add('camera-cursor');
+   } else {
+      titleOutline.classList.remove('camera-cursor');
+   }
+
+   var navTitle = document.getElementById('nav-title')
+
+   if (window.pageYOffset > 489) {
+      navTitle.classList.add("opacity-zero");
+   } else {
+      navTitle.classList.remove("opacity-zero");
    }
 
    this.console.log(window.pageYOffset)
